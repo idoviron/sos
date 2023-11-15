4c9fd0e8d7c8ba9a8cf9a8d04150edf73896cf4e
idoviron <idoviron@gmail.com>
2023-11-15 21:40:22 +0200

    fixed cors (hopefully)

--- index.js
+++ index.js
@@ -6,6 +6,25 @@ let value = "0"
 let seq = "11100011100011100010101000";
 let len = seq.length;
 
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 //Idiomatic expression in express to route and respond to a client request
 app.get('/', (req, res) => {        //get requests to the root ("/") will route here
     res.send(value);       //send the string "Hello World!" back to the client
